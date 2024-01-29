4ff2e635eea7d97960cfd514085fd2cc58d17d79
Achchutan_S <achchutan.srinivasan@gmail.com>
2024-01-29 11:39:19 +0530

    random-word appears

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>I still know react</div>;
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        // console.log(json);
+        //random number between 0 and 49
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+  return (
+    <div className="App">
+      <h1>Wordle </h1>
+      {solution && <div>Solution is {solution}</div>}
+    </div>
+  );
 };
 
 export default App;
