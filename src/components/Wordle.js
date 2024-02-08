919cfcc7ec3d0e7e603ec7be15b32645376e6575
Achchutan_S <achchutan.srinivasan@gmail.com>
2024-02-09 00:29:24 +0530

    game works

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -4,24 +4,29 @@ import { useEffect } from "react";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
 const Wordle = ({ solution }) => {
-  const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle({
-    solution,
-  });
+  const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } =
+    useWordle({
+      solution,
+    });
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
+    if (isCorrect) {
+      window.removeEventListener("keyup", handleKeyup);
+    }
+    if (turn > 5) {
+      window.removeEventListener("keyup", handleKeyup);
+    }
 
     return () => window.removeEventListener("keyup", handleKeyup);
-  }, [handleKeyup]);
-  useEffect(() => {
-    console.log(guesses, turn, isCorrect);
-  }, [guesses, turn, isCorrect]);
+  }, [handleKeyup, isCorrect, turn]);
+
   return (
     <div>
       <div>solution- {solution}</div>
       <div>current guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
-      <Keypad />
+      <Keypad usedKeys={usedKeys} />
     </div>
   );
 };
